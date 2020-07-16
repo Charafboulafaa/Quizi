a4549450a494697e88ca258941f72e3f4e7b3d40
unknown <boulafaacharaf@gmail.com>
2020-07-16 09:13:35 -0400

    added navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div className="text-2xl font-extrabold py-3 bg-teal-400 text-white tracking-wide">
+            Quizi
+        </div>
+    )
+}
