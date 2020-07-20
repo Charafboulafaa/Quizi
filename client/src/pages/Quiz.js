cda14d5344df23a7eb20cf6205f917625a918ea0
unknown <boulafaacharaf@gmail.com>
2020-07-20 10:03:24 -0400

    Builded Quiz page

--- client/src/pages/Quiz.js
+++ client/src/pages/Quiz.js
@@ -2,14 +2,15 @@ import React from 'react'
 
 export default function Quiz() {
     return (
-        <div>
-            <h1>The Coldest Sunset</h1>
-            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+        <div className="my-5">
+            <h1 className="text-5xl font-bold text-teal-600">The Coldest Sunset</h1>
 
-            <div>
-                <div>Duis gravida blandit</div>
-                <div>Duis gravida blandit</div>
-                <div>Duis gravida blandit</div>
+            <div className="my-8 p-6 text-2xl border-2 border-teal-800 rounded-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+
+            <div className="flex justify-evenly">
+                <div className="p-4 border border-teal-800 rounded-sm hover:bg-teal-700 hover:text-white">Duis gravida blandit</div>
+                <div className="p-4 border border-teal-800 rounded-sm hover:bg-teal-700 hover:text-white">Duis gravida blandit</div>
+                <div className="p-4 border border-teal-800 rounded-sm hover:bg-teal-700 hover:text-white">Duis gravida blandit</div>
             </div>
         </div>
     )
