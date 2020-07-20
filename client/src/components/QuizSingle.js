134455defacaa59639ce300903f32ba490265d57
unknown <boulafaacharaf@gmail.com>
2020-07-20 09:13:14 -0400

    added Single Quiz card

--- client/src/components/QuizSingle.js
+++ client/src/components/QuizSingle.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function QuizSingle() {
     return (
-        <div>
-            Quiz
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <img className="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains" />
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+                <p className="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                </p>
+            </div>
         </div>
     )
 }
---
+++ client/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import QuizList from '../components/QuizList'
+
+export default function Home() {
+    return (
+        <div>
+            <QuizList />
+        </div>
+    )
+}
