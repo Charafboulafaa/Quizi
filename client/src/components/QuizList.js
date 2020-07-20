cb076a57fd18cd977ebcaf661fa1c8c65005fc9d
unknown <boulafaacharaf@gmail.com>
2020-07-20 09:07:52 -0400

    added Single Quiz component

--- client/src/components/QuizList.js
+++ client/src/components/QuizList.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import QuizSingle from './QuizSingle'
 
 export default function QuizList() {
     return (
         <div className="mt-5">
-            
+            <QuizSingle />
+            <QuizSingle />
+            <QuizSingle />
+            <QuizSingle />
+            <QuizSingle />
+            <QuizSingle />
+            <QuizSingle />
         </div>
     )
 }
