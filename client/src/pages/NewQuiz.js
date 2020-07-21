c76d25d3695e40a438f62965de28e9304995c079
unknown <boulafaacharaf@gmail.com>
2020-07-21 10:12:27 -0400

    Quiz name field

--- client/src/pages/NewQuiz.js
+++ client/src/pages/NewQuiz.js
@@ -3,7 +3,7 @@ import Input from '../components/FormInput';
 
 export default function NewQuiz() {
 
-    const [quiz, setQuiz] = useState({name: ''})
+    const [quizName, setQuizName] = useState('')
     
     const [questions, setQuestions] = useState([
         {
@@ -16,6 +16,10 @@ export default function NewQuiz() {
         }
     ])
       
+    const handleQuizNameChange = (e) => {
+      setQuizName(e.target.value)
+    }
+
     const addQuestion = () => {
       setQuestions(
         [
@@ -55,6 +59,10 @@ export default function NewQuiz() {
 
     return (
         <div className="p-8 flex flex-col max-w-md mx-auto">
+
+          <div className="my-2">
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 mb-2 text-gray-900" type="text" placeholder="Quiz Name" value={quizName} onChange={(e) => handleQuizNameChange(e)}/>
+          </div>
 
         {questions.map((question, index) => (
             <Input question={question} index={index} handleChange={handleChange}/>
