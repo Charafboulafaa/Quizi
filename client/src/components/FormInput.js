e76be23388b6269f91ec036c5d10191464c6a947
unknown <boulafaacharaf@gmail.com>
2020-07-21 10:03:20 -0400

    display and handle change of questions forms

--- client/src/components/FormInput.js
+++ client/src/components/FormInput.js
@@ -2,11 +2,20 @@ import React from 'react'
 
 export default function FormInput(props) {
     return (
-        <div className="mb-4">
-            <label className="block text-left text-gray-900 text-sm font-bold mb-2">
-                {props.label}
-            </label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-900" type="text" placeholder={props.label} name={props.name}/>
+        <div className="my-2">
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 mb-2 text-gray-900" type="text" name="question" value={props.question.question} onChange={(e) => props.handleChange(e, props.index)}/>
+
+            <div className="ml-6">
+
+                {props.question.answers && props.question.answers.map((answer, index) => (
+                    <div className="flex justify-center items-center">
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 my-1 mr-2 text-gray-900" type="text" value={answer.answer}  onChange={(e) => props.handleChange(e, props.index, index)}/>
+
+                        <input type="checkbox" checked={answer.correct ? 'checked' : ''}/>
+                    </div>
+                ))}
+
+            </div>
         </div>
     )
 }
--- client/src/pages/NewQuiz.js
+++ client/src/pages/NewQuiz.js
@@ -1,20 +1,75 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Input from '../components/FormInput';
 
 export default function NewQuiz() {
+
+    const [quiz, setQuiz] = useState({name: ''})
+    
+    const [questions, setQuestions] = useState([
+        {
+          question: 'this is a question',
+          answers: [
+            { answer: 'answer one', correct:false },
+            { answer: 'answer 2', correct:true },
+            { answer: '3', correct:false }
+          ]
+        },
+        {
+          question: 'this is a question',
+          answers: [
+            { answer: 'answer one', correct:false },
+            { answer: 'answer 2', correct:false },
+            { answer: '3', correct:true }
+          ]
+        }
+    ])
+      
+    const addQuestion = () => {
+      setQuestions(
+        [
+          ...questions,
+          {
+            question: 'this is a question',
+            answers: [
+              { answer: 'answer one', correct:false },
+              { answer: 'answer 2', correct:false },
+              { answer: '3', correct:false }
+            ]
+          }
+        ]
+      )
+    }
+
+    const handleChange = (e, questionIndex, answerIndex) => {
+      
+        const { name, value } = e.target
+        let currentQuestion = questions[questionIndex]
+
+        if (name == 'question') {
+            currentQuestion.question = value
+        }
+        else {
+            currentQuestion.answers[answerIndex].answer = value
+        }
+
+        let editedQuestions = questions.map((question, index) => (
+          questionIndex == index ? currentQuestion : question 
+        ))
+
+        setQuestions(editedQuestions)
+    }
+
+        
+
     return (
         <div className="p-8 flex flex-col max-w-md mx-auto">
-            <Input label="Quiz Name" name="quiz_name"/>
-            
-            <Input label="Question" name="question"/>
-            
-            <div>
-                <Input label="Answer 1" name="answer"/>
-                <Input label="Answer 2" name="answer"/>
-                <Input label="Answer 3" name="answer"/>
-            </div>
 
-            <button>add Question</button>
+        {questions.map((question, index) => (
+            <Input question={question} index={index} handleChange={handleChange}/>
+        ))}
+
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={addQuestion}>Add Question</button>
+
         </div>
     )
 }
