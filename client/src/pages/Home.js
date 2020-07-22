c3913520a53178c9bf3ad27b3a4c6ccea823ae31
unknown <boulafaacharaf@gmail.com>
2020-07-22 02:02:02 -0400

    new quiz btn in home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,16 @@
-import React from 'react'
-import QuizList from '../components/QuizList'
+import React from 'react';
+import QuizList from '../components/QuizList';
+import {Link} from 'react-router-dom'
 
 export default function Home(props) {
     return (
         <div>
-           {props.currentUser && <QuizList setQuizzes={props.setQuizzes} quizzes={props.quizzes} currentUser={props.currentUser}/>}
+            <div className="my-3 flex justify-end">
+                <Link to="/new" className="bg-green-400 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">New Quiz</Link>
+            </div>
+            <div>
+            {props.currentUser && <QuizList setQuizzes={props.setQuizzes} quizzes={props.quizzes} currentUser={props.currentUser}/>}
+            </div>
         </div>
     )
 }
